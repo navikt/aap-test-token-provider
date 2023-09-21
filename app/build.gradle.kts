3b6d6bf9b5c9bad4c0c6b1ed98cada73112016f1
VHollund <vetlehoo@gmail.com>
2023-09-21 09:48:20 +0200

    Endret filstruktue for å få dockerfile til å kjøre riktig
    Co-authored-by: Richard Borge <richard.borge@nav.no>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,3 +52,8 @@ tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
+kotlin.sourceSets["main"].kotlin.srcDirs("main")
+kotlin.sourceSets["test"].kotlin.srcDirs("test")
+sourceSets["main"].resources.srcDirs("main")
+sourceSets["test"].resources.srcDirs("test")
