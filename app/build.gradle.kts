b58840a13b0f8c10df8db36d19402cc23b394bc8
Fredrik Meyer <fredrik.meyer@nav.no>
2024-10-18 11:37:38 +0200

    Downgrade: bruk samme ktor-versjon som aap-libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ktorVersion = "3.0.0"
+val ktorVersion = "2.3.9"
 
 plugins {
     kotlin("jvm") version "2.0.21"
-    id("io.ktor.plugin") version "3.0.0"
+    id("io.ktor.plugin") version "2.3.9"
     application
 }
 
