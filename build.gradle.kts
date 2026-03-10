3372e7613404e6d86408fc101ed72ff9e3714a99
Per Henden <per.christian.henden@nav.no>
2026-03-10 09:41:49 +0100

    Støtt multi-project build
    [ci skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 
 // Call the tasks of the subprojects
-for (taskName in listOf<String>("clean", "build", "check")) {
+for (taskName in listOf<String>("clean", "build", "assemble", "check")) {
     tasks.named(taskName) {
         dependsOn(subprojects.map { it.path + ":$taskName" })
     }
